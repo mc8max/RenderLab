ec1b8a00af5c2daf4a3acdbeb5c9ae3a03be38f6
Mc8Max <mc8max@gmail.com>
2026-02-27 09:08:49 +0700

    Initial Commit

---
+++ RenderLab/RenderLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenderLabApp.swift
+//  RenderLab
+//
+//  Created by Hoàng Trí Tâm on 27/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RenderLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
